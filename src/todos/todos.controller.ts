785d9a341d655590910cbcfd19228d6846bf21d1
Suhas S Murthy <murthy.suhas@gmail.com>
2021-08-21 23:25:26 +0530

    routes added (#3)

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  @Post()
+  createTodo(@Body() body: any) {
+    console.log('Create TODO ', body);
+  }
+  @Get()
+  findTodos() {
+    console.log('Get all todos');
+  }
+  @Get('/:id')
+  findTodoById(@Param('id') id: string) {
+    console.log(`Get Todo by Id ${id}`);
+  }
+  @Patch('/:id')
+  updateTodoStatus(@Param('id') id: string) {
+    console.log(`Update Todo by Id ${id}`);
+  }
+}
