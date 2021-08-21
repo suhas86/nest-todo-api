9e6176e74da5b4d06c3e6b0b9ec6dfd24a1f9196
Suhas S Murthy <murthy.suhas@gmail.com>
2021-08-22 01:14:40 +0530

    validation added (#5)

---
+++ src/todos/dtos/create-todo.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
+  todo: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
