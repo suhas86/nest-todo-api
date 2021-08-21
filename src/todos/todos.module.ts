2f62f8c306619fc981f245fc727066555de8b216
Suhas S Murthy <murthy.suhas@gmail.com>
2021-08-21 23:00:39 +0530

    entity added (#2)

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosController } from './todos.controller';
+import { Todos } from './todos.entity';
 import { TodosService } from './todos.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todos])],
   controllers: [TodosController],
   providers: [TodosService],
 })
